54b8f288ffafb311e04f354250f477a34f61d235
Riadh mouamnia <riad.sodmg@gmail.com>
2023-03-06 12:17:45 +0100

    move tests into thier folders

--- __tests__/AuthContext-test.tsx
+++
@@ -1,9 +0,0 @@
-import {AuthContext} from 'context/AuthContext';
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-describe('AuthContext', () => {
-  it('renders without crashing', () => {
-    renderer.create(<AuthContext.Provider value={{}} />);
-  });
-});
